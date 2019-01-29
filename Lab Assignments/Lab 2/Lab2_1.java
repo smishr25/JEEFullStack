be643426517a84bbccdbf0e0814384e0d7f46129
sushilsingh23 <35341499+sushilsingh23@users.noreply.github.com>
2019-01-29 15:18:04 +0530

    Add files via upload

---
+++ Lab Assignments/Lab 2/Lab2_1.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+public class Lab2_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String fname,lname,gen;
+		int age;
+		double weight;
+		Scanner s=new Scanner(System.in);
+		System.out.println("Enter First Name: ");
+		fname=s.nextLine();
+		System.out.println("Enter Last Name: ");
+		lname=s.nextLine();
+		System.out.println("Enter Gender (M/F): ");
+		gen=s.nextLine();
+		System.out.println("Enter Age: ");
+		age=s.nextInt();
+		System.out.println("Enter Weight: ");
+		weight=s.nextDouble();
+		System.out.println("First Name: "+fname+"\n");
+		System.out.println("Last Name: "+lname+"\n");
+		System.out.println("Gender: "+gen+"\n");
+		System.out.println("Age: "+age+"\n");
+		System.out.println("Weight: "+weight+"\n");
+	}
+
+}
+/*OUTPUT
+Enter First Name: 
+Sushil
+Enter Last Name: 
+Singh
+Enter Gender (M/F): 
+M
+Enter Age: 
+22
+Enter Weight: 
+70
+First Name: Sushil
+
+Last Name: Singh
+
+Gender: M
+
+Age: 22
+
+Weight: 70.0
+*/
